cde3f915a12a4c04e8eccc460fcb6e1d778720b4
Khoa D. Vo <khoa.dante@gmail.com>
2022-05-23 12:00:50 +0700

    add model optimizers utils

---
+++ utils/model_optimizers.py
@@ -0,0 +1,23 @@
+import config
+from torch import nn
+from torch import optim
+from typing import List
+
+
+def define_asrnet_optimizer(model) -> optim.AdamW:
+    optimizer = optim.AdamW(model.parameters(), config.model_lr, config.model_betas)
+
+    return optimizer
+
+
+def define_asrgan_optimizer(
+    generator: nn.Module, discriminator: nn.Module
+) -> List[optim.AdamW, optim.AdamW]:
+    g_optimizer = optim.AdamW(
+        generator.parameters(), config.model_lr, config.model_betas
+    )
+    d_optimizer = optim.AdamW(
+        discriminator.parameters(), config.model_lr, config.model_betas
+    )
+
+    return g_optimizer, d_optimizer

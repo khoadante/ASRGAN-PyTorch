7ba0cbc8a7aeec5d9916ac5b8a551024805087de
Khoa D. Vo <khoa.dante@gmail.com>
2022-05-23 12:05:20 +0700

    add model schedulers utils

---
+++ utils/model_schedulers.py
@@ -0,0 +1,24 @@
+import config
+from torch import optim
+from typing import List
+
+
+def define_asrnet_scheduler(optimizer) -> optim.lr_scheduler.StepLR:
+    scheduler = optim.lr_scheduler.StepLR(
+        optimizer, config.lr_scheduler_step_size, config.lr_scheduler_gamma
+    )
+
+    return scheduler
+
+
+def define_asrgan_scheduler(
+    g_optimizer: optim.AdamW, d_optimizer: optim.AdamW
+) -> List[optim.lr_scheduler.MultiStepLR, optim.lr_scheduler.MultiStepLR]:
+    g_scheduler = optim.lr_scheduler.MultiStepLR(
+        g_optimizer, config.lr_scheduler_milestones, config.lr_scheduler_gamma
+    )
+    d_scheduler = optim.lr_scheduler.MultiStepLR(
+        d_optimizer, config.lr_scheduler_milestones, config.lr_scheduler_gamma
+    )
+
+    return g_scheduler, d_scheduler
